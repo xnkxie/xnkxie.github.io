b8e0fe158f9da19592317ef227872d45c07aa1da
xnkxie <1370134404@qq.com>
2021-05-12 16:06:04 +0800

    1

---
+++ Javascript/index.js
@@ -0,0 +1,30 @@
+// 记录出一条信息
+// console.log(100)
+// 定义一个数据类型
+let myName = '注册成功' //字符串类型
+console.log(myName)
+//字符串连接
+let age = '我的年龄:18'
+let wold = myName + age
+console.log(wold)
+
+let score = 100 //数字类型
+// + - * / %
+console.log(score % 3)
+
+
+let isFask = true //布尔值类型 true false
+console.log(isFask)
+
+let bag = undefined //空，
+console.log(bag)
+
+let bag2 = null
+console.log(null)
+
+
+//let var const 保证变量不会被修改
+
+
+
+
---
+++ Javascript/index1.js
@@ -0,0 +1,51 @@
+//数组 Array
+let classA = ['小米', '小白', '小红']
+console.log('这学期: ', classA)
+// 数组语法 push  往最后的数组添加东西
+classA.push('小伙')
+console.log('下学期: ',classA)
+
+let igPhotos = [
+    'https://xx.img.com/a1.png',
+    'https://xx.img.com/a2.png',
+    'https://xx.img.com/a3.png'
+]
+console.log('igPhotos',igPhotos)
+//变量的长度
+console.log(classA.length)
+
+//项目 object
+const card = {
+    name:'实现需求',
+    addres: '江西省' ,
+    age: 18
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
